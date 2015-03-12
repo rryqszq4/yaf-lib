da88998a3fdf3711f9af8e3d1fb6690ac441f563
rryqszq4 <279664262@163.com>
2015-03-12 12:03:04 +0800

    [controller] add error

---
+++ application/controllers/Error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @name ErrorController
+ * @desc   错误控制器, 在发生未捕获的异常时刻被调用
+ * @see    http://www.php.net/manual/en/yaf-dispatcher.catchexception.php
+ * @author chenzhidong
+ */
+class ErrorController extends Controller {
+    //从2.1开始, errorAction支持直接通过参数获取异常
+    public function errorAction($exception) {
+        //1. assign to view engine
+        $this->_view->assign("message", $exception->getMessage());
+        return true;
+    }
+}
