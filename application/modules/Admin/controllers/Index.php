a279072c85360bf885939a290f66c0792be444a8
rryqszq4 <279664262@163.com>
2015-03-12 11:48:42 +0800

    [module] add admin module

---
+++ application/modules/Admin/controllers/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoquan
+ * Date: 15-3-12
+ * Time: 上午11:37
+ */
+class IndexController extends Controller {
+
+    public function IndexAction(){
+        echo "[moduel admin]";
+    }
+}
