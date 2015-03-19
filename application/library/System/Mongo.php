0c8552ad4be46e9cc3cc71d42b2fb48f39c157bc
rryqszq4 <279664262@163.com>
2015-03-19 17:51:21 +0800

    [mongo] update

--- application/library/System/Mongo.php
+++ application/library/System/Mongo.php
@@ -38,7 +38,7 @@ class System_Mongo {
 
     /**
      * Description:静态方法，单例访问统一入口
-     * @return Singleton：返回应用中的唯一对象实例
+     * @return System_Mongo：返回应用中的唯一对象实例
      */
     public static function getInstance()
     {
