e964c7cf1f5f70f34271b60706b7b8c55c4c55c3
rryqszq4 <279664262@163.com>
2015-03-30 18:13:48 +0800

    [bootstrap]  add routes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -15,7 +15,8 @@ class Bootstrap extends Yaf_Bootstrap_Abstract{
     }
 
 	public function _initRoute(Yaf_Dispatcher $dispatcher){
-	
+	    $router = Yaf_Dispatcher::getInstance()->getRouter();
+        $router->addConfig($this->_config->routes);
 	}
 
     public function _initLayout(Yaf_Dispatcher $dispatcher){
