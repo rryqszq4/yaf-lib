3d52f97e384feeb58f75b359cfa1a581f056443c
rryqszq4 <279664262@163.com>
2015-05-25 18:12:53 +0800

    [hprose] httpserver and httpclient

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
+#xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
@@ -11,20 +11,23 @@ if (DEBUG_TOOLS){
     #DebugTools::startTimeMemory();
 }
 
+require_once(APPLICATION_PATH."/application/tools/hprose-php/Hprose.php");
+
 $app = new Yaf_Application(APPLICATION_PATH."/conf/application.ini");
 $app->bootstrap()->run();
 
 if (DEBUG_TOOLS){
     #DebugTools::useTimeMemory();
 }
-$xhprof_data = xhprof_disable();
 
-$XHPROF_ROOT = APPLICATION_PATH."/application/tools";
-include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+#$xhprof_data = xhprof_disable();
 
-$xhprof_runs = new XHProfRuns_Default();
-$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+#$XHPROF_ROOT = APPLICATION_PATH."/application/tools";
+#include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+#include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
 
-echo "http://192.168.80.138/xhprof/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
+#$xhprof_runs = new XHProfRuns_Default();
+#$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+
+#echo "http://192.168.80.138/xhprof/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
 ?>
