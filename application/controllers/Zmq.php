bca0fee21eeb1f7d14f40b227c4b533d5af73a97
rryqszq4 <279664262@163.com>
2015-07-28 11:44:05 +0800

    [zmq] pub-sub mode

--- application/controllers/Zmq.php
+++ application/controllers/Zmq.php
@@ -49,4 +49,42 @@ class ZmqController extends Sontroller
             $responder->send("World");
         }
     }
+
+    public function publisherAction(){
+        $context = new ZMQContext();
+
+        $publisher = $context->getSocket(ZMQ::SOCKET_PUB);
+
+        $publisher->bind("tcp://127.0.0.1:5556");
+
+        while(true){
+            $zipcode = mt_rand(0, 100000);
+            $temperature = mt_rand(-80, 135);
+            $relhumidity = mt_rand(10, 60);
+
+            $update = sprintf("%05d %d %d", $zipcode, $temperature, $relhumidity);
+
+            $publisher->send($update);
+        }
+    }
+
+    public function subscriberAction(){
+        $context = new ZMQContext();
+
+        echo "Collecting updates from weather server...",PHP_EOL;
+        $subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
+        $subscriber->connect("tcp://127.0.0.1:5556");
+
+        $filter = "1001";
+        $subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $filter);
+
+        $total_temp = 0;
+        for($update_nbr = 0; $update_nbr < 100; $update_nbr++)
+        {
+            $string = $subscriber->recv();
+            sscanf($string, "%d %d %d", $zipcode, $temperature, $relhumidity);
+            $total_temp += $temperature;
+        }
+        printf("Average temperature for zipcode '%s' was %dF\n", $filter, (int)($total_temp/$update_nbr));
+    }
 }
