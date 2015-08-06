fb658c184efa329021a0b6e890d8b298b1e380e8
rryqszq4 <279664262@163.com>
2015-08-06 18:56:35 +0800

    [reactjs] reactjs examle

---
+++ application/controllers/Reactjs.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoquan
+ * Date: 15-8-5
+ * Time: 下午2:31
+ */
+
+class ReactjsController extends Controller {
+
+    public $_layout = "reactjs_layout";
+
+    public function indexAction(){
+
+    }
+}
